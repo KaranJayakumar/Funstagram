c5af173ce75692c3fb02c91f9d553b2f298a138b
KaranJayakumar <karanjaykumar@gmail.com>
2024-03-08 12:24:08 +1100

    Updated file structure

---
+++ src/root/App.tsx
@@ -0,0 +1,12 @@
+import {Routes, Route} from 'react-router-dom';
+const App = () => {
+    return (
+        <main className = 'flex h-screen'>
+        <Routes>
+        <Route path = '/sign-in' element = {<SignInForm/>}/>
+        <Route index element = {<Home/>}/>
+        </Routes>
+        </main>
+    )
+}
+export default App;
---
+++ src/root/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import './globals.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+
+    <App />
+    </BrowserRouter>
+);
+
